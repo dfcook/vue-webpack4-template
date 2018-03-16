d38a2021fe94bdd5906fe5c0f6cd75a5772c3c25
dfcook <dfcook@hotmail.com>
2018-03-16 18:14:18 +0000

    Move tests to jest and fix

--- test/specs/WelcomeMessage.spec.js
+++ test/specs/WelcomeMessage.spec.js
@@ -1,13 +1,14 @@
-import { shallow } from 'vue-test-utils'
-import WelcomeMessage from 'components/Home/WelcomeMessage.vue'
+import { shallow } from '@vue/test-utils'
+import WelcomeMessage from '@/components/Home/WelcomeMessage'
 
 describe('WelcomeMessage.vue', () => {
   it('renders props.msg when passed', () => {
-    const name = 'new message'
+    const name = 'tester'
+
     const wrapper = shallow(WelcomeMessage, {
       propsData: { name }
     })
 
-    expect(wrapper.text()).toBe(name)
+    expect(wrapper.text()).toBe('Hello tester from my Vue.js page, built with Webpack 4!')
   })
 })
